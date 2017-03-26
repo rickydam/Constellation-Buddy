42013f7604202656a5ce6eb150de774a80cebf2c
Adan Moran-MacDonald <adanmoranm@gmail.com>
2017-03-26 00:43:37 -0400

    Added getMessage() and tried to write to NI myRIO

--- SocketClient.java
+++ SocketClient.java
@@ -10,27 +10,38 @@ import java.util.Map;
  * TCP client to connect to LabView
  */
 public class SocketClient {
-	final int PORT = 6123;
-	final String ADDRESS = "172.16.0.10";
-	Socket client = null;
-	
-	// Hashmap of all constellations and their coordinate ranges
-	// Coordinates are stored as {xmin, ymin, xmax, ymax}
-	@SuppressWarnings("serial")
-	final Map<String, Integer[]> constellations = new HashMap<String, Integer[]>() {{
-		Integer[] coordinates = {0, 0, 100, 100};
-		put("Little Dipper", coordinates);
-	}};
+    final int PORT = 6123;
+    final String ADDRESS = "172.16.0.10";
+    Socket client = null;
+    
+    // Hashmap of all constellations and their coordinate ranges
+    // Coordinates are stored as {xmin, ymin, xmax, ymax}
+    @SuppressWarnings("serial")
+    final Map<String, Integer[]> constellations = new HashMap<String, Integer[]>() {{
+        Integer[] coordinates = {0, 0, 100, 100};
+        put("Little Dipper", coordinates);
+    }};
 
     /**
      * Start the server and wait for incoming connections
      * @throws IOException
      */
     public void startTCP() throws IOException {
-    	client = new Socket(ADDRESS, PORT); // Connect to LabView server
-    	BufferedReader input = new BufferedReader(new InputStreamReader(client.getInputStream()));
-    	String answer = input.readLine(); // Read input stream from the server
-    	System.out.println(answer);
+        client = new Socket(ADDRESS, PORT); // Connect to LabView server
+    }
+
+    public String getMessage() {
+        if (client != null) {
+            try {
+                BufferedReader input = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                String answer = input.readLine();
+		return answer;
+            }
+           catch(IOException e)
+           {
+           }
+       }
+       return "";
     }
     
     /**
@@ -38,18 +49,18 @@ public class SocketClient {
      * @param msg
      */
     public void sendMessage(String msg) {
-    	if(client != null) {
-    	    try {
-    	    	// Connected, so try sending a message to the server
-        		DataOutputStream out = new DataOutputStream(client.getOutputStream());
-				out.writeUTF("Hi");
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-    	} else {
-    		// Not connected to the server
-    		System.err.println("Send message failed: Client not connected.");
-    	}
+        if(client != null) {
+            try {
+                // Connected, so try sending a message to the server
+                DataOutputStream out = new DataOutputStream(client.getOutputStream());
+                out.writeUTF("Hi");
+            } catch (IOException e) {
+		    System.out.println("Failed send :( " + e.toString());
+            }
+        } else {
+            // Not connected to the server
+            System.err.println("Send message failed: Client not connected.");
+        }
     }
     
     /**
@@ -58,9 +69,9 @@ public class SocketClient {
      * @param y
      */
     public void checkConstellation(int x, int y) {
-    	for(String constellation : constellations.keySet()) {
-    			
-    		
-    	}
+        for(String constellation : constellations.keySet()) {
+                
+            
+        }
     }
 }
